26f24c79e0888fac28de5c1cc93d5275053251a9
JghJaime <palomojr01@gmail.com>
2024-06-10 13:10:09 +0200

    formateo

--- premio-clientes.js
+++ premio-clientes.js
@@ -1,33 +1,33 @@
 function acmeRegalo(clientes) {
-  var premioNavidad;
-  console.log("");
+    var premioNavidad;
+    console.log("");
 
-  for (i = 0; i <= clientes.length - 1; i++) {
-    var cuantia = Math.floor(Math.random() * 15000);
-    if (cuantia > 9999) {
-      premioNavidad = "Cesta de navidad con jamón";
-    } else if (cuantia >= 5000) {
-      premioNavidad = "Cesta de navidad simplona";
-    } else {
-      premioNavidad = "Carta y pegatina de Acme";
+    for (i = 0; i <= clientes.length - 1; i++) {
+        var cuantia = Math.floor(Math.random() * 15000);
+        if (cuantia > 9999) {
+            premioNavidad = "Cesta de navidad con jamón";
+        } else if (cuantia >= 5000) {
+            premioNavidad = "Cesta de navidad simplona";
+        } else {
+            premioNavidad = "Carta y pegatina de Acme";
+        }
+
+        console.log(
+            clientes[i] + ": " + cuantia + " eur (" + premioNavidad + ")"
+        );
     }
 
-    console.log(
-      clientes[i] + ": " + cuantia + " eur (" + premioNavidad + ")"
-    );
-  }
-  
-  console.log("");
+    console.log("");
 }
 acmeRegalo([
-  "Juan Ramos",
-  "Pedro Sanchez",
-  "Begoña Gómez",
-  "Frederick Williams",
-  "Roberta Williams",
-  "Paco Porras",
-  "La Veneno",
-  "Profesor Moriarti",
-  "Pato Lucas",
-  "Cristobal Montoro",
+    "Juan Ramos",
+    "Pedro Sanchez",
+    "Begoña Gómez",
+    "Frederick Williams",
+    "Roberta Williams",
+    "Paco Porras",
+    "La Veneno",
+    "Profesor Moriarti",
+    "Pato Lucas",
+    "Cristobal Montoro",
 ]);
